32834abd5bc5faaf85ec7e070675202068fa17e9
Michael Mosmann <michael@mosmann.de>
2021-11-06 09:13:06 +0100

    organize imports

--- src/main/java/de/flapdoodle/transition/init/Edge.java
+++ src/main/java/de/flapdoodle/transition/init/Edge.java
@@ -19,7 +19,6 @@ package de.flapdoodle.transition.init;
 import de.flapdoodle.transition.StateID;
 import de.flapdoodle.transition.initlike.State;
 import de.flapdoodle.transition.initlike.resolver.StateOfNamedType;
-import org.immutables.value.Value;
 
 import java.util.function.Function;
 
